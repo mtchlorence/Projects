03fa98fcf46b4797428aa99ba49c6dd10e143008
Mitch <mitchlorencedelapiza@gmail.com>
2026-06-07 20:33:18 +0800

    Add dockerfile

--- Cloud_Engineering/resume_analyzer_monitoring/app/app.py
+++ Cloud_Engineering/resume_analyzer_monitoring/app/app.py
@@ -63,4 +63,4 @@ def metrics():
     return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
